d71d8cd33927f36793dd8350aee58e666d635ce3
Rigidity <me@rigidnetwork.com>
2024-06-24 08:37:51 -0400

    Remove feature gating

--- dep-crate/src/example.rs
+++ dep-crate/src/example.rs
@@ -1,11 +1,10 @@
-#[cfg_attr(feature = "python", pyo3::pyclass)]
+use pyo3::*;
+
+#[pyclass]
 pub struct Example {}
 
-#[cfg(feature = "python")]
 mod submodule {
     use super::*;
-
-    use pyo3::*;
 
     #[pymethods]
     impl Example {
